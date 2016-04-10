85ac2af8a26db10be2d3f9a28ccb2b412ad5e812
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-04-10 17:38:42 -0600

    commenting app config file; moving assets dir

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session'); // ADDITION!
-var flash = require('connect-flash'); // ADDITION!
-var moment = require('moment-timezone'); // ADDITION!
+var session = require('express-session'); // ADDITION! enables session storage; required for flash messages
+var flash = require('connect-flash'); // ADDITION! enables flash messages
+var moment = require('moment-timezone'); // ADDITION! enables date string formatting
 
 var home_routes = require('./app/controllers/home_controller'); // EDIT! was: var routes = require('./routes/index');
 var robot_routes = require('./app/controllers/robots_controller'); // EDIT! was: var users = require('./routes/users');
 
-var sessionStore = new session.MemoryStore; // ADDITION!
+var sessionStore = new session.MemoryStore; // ADDITION! the default memory store for sessions in the development environment
 
 var app = express();
 
@@ -19,7 +20,7 @@ app.locals.moment = moment;  // ADDITION! this makes moment available as a varia
 app.locals.title = "Robots App!" // ADDITION! set a common title for all EJS views
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app/views')); // EDIT! was: app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views')); // EDIT! recognizes view templates stored in the app/views directory. was: app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -28,7 +29,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'assets'))); // EDIT! was: app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/assets'))); // EDIT! recognizes static files stored in the app/assets directory. was: app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
    cookie: { maxAge: 60000 },
@@ -39,16 +40,15 @@ app.use(session({
    saveUninitialized: true
  })); // ADDITION!
 
-app.use(flash()); // ADDITION!
+app.use(flash()); // ADDITION! enables the application to use the flash module
 
-// include flash messages (must be placed below session and cookie parser, and above ... app.use('/', routes);)
 app.use(function (req, res, next) {
  res.locals.messages = require('express-messages')(req, res);
  next();
-}); // ADDITION!
+}); // ADDITION! enables storage of flash messages and makes them accessable to views. must be placed below app.use(cookieParser()) section, and above app.use('/', routes) section
 
-app.use('/', home_routes); // EDIT! was: app.use('/', routes);
-app.use('/', robot_routes); // EDIT! was: app.use('/users', users);
+app.use('/', home_routes); // EDIT! orients the location of home paths relative to the root url, "/". was: app.use('/', routes);
+app.use('/', robot_routes); // EDIT! orients the location of robot paths relative to the root url, "/". some people might want to orient these reletive to "/robots" instead, in which case you'd have to remove "robots/" from your robots controller paths. was: app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -64,7 +64,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('_error', { // EDIT! was: res.render('error', {
+    res.render('_error', { // EDIT! recognizes a renamed (underscorized) view template for errors. file name was: res.render('error', {
       message: err.message,
       error: err
     });
@@ -75,11 +75,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('_error', { // EDIT! was: res.render('error', {
+  res.render('_error', { // EDIT! recognizes a renamed (underscorized) view template for errors. file name was: res.render('error', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
